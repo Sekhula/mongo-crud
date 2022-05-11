1961b547deac8c0bb257e864736ea1855bb23f76
Tebalelo Sekhula <tebalelo.sekhula@dadevs.co.za>
2022-05-11 16:31:06 +0200

    Make it better

--- backend/app/configs/db.config.js
+++ backend/app/configs/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/chat_db"
+    // url: "mongodb://localhost:27017/chat_db"
+    url: "mongodb+srv://tebalelo:Tebalelo11@cluster0.gh28k.mongodb.net/Edu_App?retryWrites=true&w=majority"
   };
